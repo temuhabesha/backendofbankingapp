5ab964f963989ae848c51d53673229b9b3bff394
tomhabesha <temesgenwondim@gmail.com>
2025-01-06 00:26:28 +0300

    commit of banking app

--- controller/accountcontroller.js
+++ controller/accountcontroller.js
@@ -1,7 +1,12 @@
 async function openaccount(req,res){
-    const {fullname,fathername,
-        mothername,Nationality,dateofbirth,birthplace,regioncity,Region,zone,Woreda,Kebele,Mobile,Gender,mritalstatus,accountnumber,accounttype,employdetail,username,email,password
+    const {fullname,fathername, mothername,Nationality,dateofbirth,birthplace,regioncity,Region,zone,Woreda,Kebele,Mobile,Gender,mritalstatus,accountnumber,accounttype,employdetail,username,email,password
     } = req.body;
+
+
+    if(!fullname || !fathername || !mothername || !Nationality || !dateofbirth || !birthplace || !regioncity || !Region || !zone || !Woreda || !Kebele || !Mobile || !Gender || !mritalstatus || !accountnumber || !accounttype || !employdetail || !username || !email || !password){
+        return console.log('please fiel all filds')
+    }
+
     console.log({fullname,fathername,mothername,Nationality,dateofbirth,birthplace,Region,regioncity,zone,Woreda,Kebele,Mobile,Gender,mritalstatus,accountnumber,accounttype,employdetail,username,email,password})
 
 }
